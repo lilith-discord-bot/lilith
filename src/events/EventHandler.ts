c34b5193df85af0d91158215275ba12eece2f2d1
Théo <43091603+glazk0@users.noreply.github.com>
2023-06-09 14:09:07 +0200

    Fixed & refactor EventHandler, InteractionHandler

--- src/events/EventHandler.ts
+++ src/events/EventHandler.ts
@@ -7,20 +7,19 @@ import { Client } from '../core/Client';
 import { Event } from '../core/Event';
 
 export default class EventHandler {
+
   /**
    * The client.
    * @type {Client}
    * @readonly
    */
-  public readonly client: Client;
-
+  readonly client: Client;
   /**
    * The event handlers.
    * @type {Collection<string, Event>}
    * @readonly
    */
-  public readonly handlers: Collection<Events, Event>;
-
+  readonly handlers: Collection<Events, Event>;
   /**
    * Creates a new event handler.
    *
@@ -36,22 +35,19 @@ export default class EventHandler {
   /**
    * Initializes the event handlers.
    */
-  public async init(): Promise<void> {
+  async init(): Promise<void> {
 
     const dir = path.join(__dirname, '..', 'events');
 
     let files = await fs.readdir(dir);
 
-    files = files.filter(
-      (file) => file.endsWith('.js') && !file.toLowerCase().includes('handler'),
-    );
+    files = files.filter((file) => file.endsWith('.js') && !file.startsWith('EventHandler'));
 
-    this.handlers.clear();
+    if (this.handlers.size > 0) this.handlers.clear();
 
     for (const file of files) {
-      const handler = new (await import(path.join(dir, file))).default(
-        this.client,
-      ) as Event;
+
+      const handler = new (await import(path.join(dir, file))).default(this.client) as Event;
 
       this.handlers.set(handler.event, handler);
     }
@@ -65,8 +61,8 @@ export default class EventHandler {
    * @param id - The event identifier.
    * @param args - The arguments.
    */
-  public async run(event: Events, ...args: any[]): Promise<void> {
-    
+  async run(event: Events, ...args: any[]): Promise<void> {
+
     const handler = this.handlers.get(event);
 
     if (!handler) throw new Error(`Event handler ${event} doesn't exist`);
---
+++ src/events/InteractionHandler.ts
@@ -0,0 +1,142 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+import { Collection, CommandInteraction, Events } from 'discord.js';
+
+import { Client } from '../core/Client';
+import { Event } from '../core/Event';
+import { Context, Interaction } from '../core/Interaction';
+
+// TODO : Refactor this, it works for now
+
+export default class InteractionHandler extends Event {
+  /**
+   * The interactions collection.
+   *
+   * @type {Collection<string, any>}
+   * @readonly
+   */
+  readonly interactions = new Collection<string, any>();
+
+  constructor(client: Client) {
+    super(client, 'onInteraction', Events.InteractionCreate);
+
+    this.interactions = new Collection<string, any>();
+
+    this.init();
+  }
+
+  /**
+   * Initializes the interaction handler.
+   *
+   * @returns {Promise<void>} - Returns nothing.
+   */
+  async init(): Promise<void> {
+    const dir = path.join(__dirname, '..', 'interactions');
+
+    let loadedCommands = [];
+
+    let files = await fs.readdirSync(dir);
+
+    if (!files) return;
+
+    const categories = files.filter?.((file) => !file.endsWith('.js'));
+
+    categories.forEach((category) => {
+      files = files.concat(
+        fs
+          .readdirSync(path.join(dir, category))
+          .map((file) => path.join(path.resolve(dir, category, file))),
+      );
+    });
+
+    loadedCommands = (
+      await Promise.all(
+        files.map(async (file) => {
+          if (!file.endsWith('.js')) return undefined;
+          try {
+            const Handler = (await import(file)).default;
+            return Handler.prototype instanceof Interaction
+              ? Handler
+              : undefined;
+          } catch (error) {
+            this.client.logger.error(error);
+            return undefined;
+          }
+        }),
+      )
+    ).filter((handler) => handler) as any[];
+
+    loadedCommands
+      .filter((command) => command.enabled)
+      .flat()
+      .filter((c) => c)
+      .forEach((command) => {
+        this.interactions.set(command.command.name, command);
+      });
+
+    this.create();
+
+    this.client.logger.info(`Loaded ${this.interactions.size} interactions`);
+  }
+
+  private async create(): Promise<void> {
+    const ready = this.client.readyAt
+      ? Promise.resolve()
+      : new Promise((resolve) => this.client.once('ready', resolve));
+
+    await ready;
+
+    for (const [name, command] of this.interactions) {
+      try {
+        await this.client.application?.commands.create(command.command);
+      } catch (error) {
+        this.client.logger.error(
+          `Failed to load interaction ${name}: ${error}`,
+        );
+      }
+    }
+  }
+
+  /**
+   * Runs the interaction.
+   *
+   * @param interaction - The interaction.
+   *
+   * @returns {Promise<void>} - Returns nothing.
+   */
+  async run(interaction: CommandInteraction): Promise<void> {
+    if (!this.client.isReady) return undefined;
+    if (!interaction) return undefined;
+
+    if (!this.interactions.has(interaction.commandName)) return undefined;
+
+    const command = this.interactions.get(interaction.commandName);
+
+    if (!command) return undefined;
+
+    let context = {} as Context;
+
+    context.client = this.client;
+
+    if (interaction.isChatInputCommand()) {
+      try {
+        await command.run?.(interaction, context);
+      } catch (error) {
+        this.client.logger.error(
+          `Failed to run interaction ${interaction.commandName}: ${error}`,
+        );
+      }
+    }
+
+    if (interaction.isAutocomplete()) {
+      try {
+        await command.autocomplete?.(interaction, context);
+      } catch (error) {
+        this.client.logger.error(
+          `Failed to run autocomplete for interaction ${command.name}: ${error}`,
+        );
+      }
+    }
+  }
+}
