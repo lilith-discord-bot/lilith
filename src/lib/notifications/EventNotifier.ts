6d1677e2af9596039d0192625c9cb74e7d8a618c
Théo <43091603+glazk0@users.noreply.github.com>
2023-06-14 23:24:39 +0200

    Added 2m delay on event

--- src/lib/notifications/EventNotifier.ts
+++ src/lib/notifications/EventNotifier.ts
@@ -67,7 +67,7 @@ export class EventNotifier {
 
       if (!cache || cachedEvent.timestamp !== value.timestamp) {
 
-        const date = Date.now();
+        const date = Date.now() - duration.minutes(2);
         const event = new Date(value.timestamp * 1000).getTime();
 
         if (date > event) {
@@ -127,96 +127,6 @@ export class EventNotifier {
   }
 }
 
-// const guilds = await this.client.repository.guild.getAll();
-
-// this.client.logger.info(`Found ${guilds.length} guilds.`);
-
-// for (const guild of guilds) {
-
-//   this.client.logger.info(`Checking guild ${guild.id}...`);
-
-//   for (let [key, value] of Object.entries(events)) {
-
-//     this.client.logger.info(`Checking event ${key}...`);
-
-//     const isCached = await this.client.cache.exists(`events:${key}`);
-
-//     if (!isCached)
-//       await this.client.cache.set(`events:${key}`, JSON.stringify(value));
-
-//     let cache = await this.client.cache.get(`events:${key}`);
-
-//     const cachedEvent = JSON.parse(cache!) as Event;
-
-//     if (cachedEvent.timestamp !== value.timestamp) {
-
-//       this.client.logger.info(`Event ${key} is outdated, updating...`);
-
-//       await this.client.cache.set(`events:${key}`, JSON.stringify(value));
-
-//       this.client.logger.info(`Event ${key} has been updated.`);
-
-//       const date = Date.now();
-//       const event = new Date(value.timestamp * 1000).getTime();
-
-//       if (date > event) {
-//         this.client.logger.info(`Event ${key} is outdated, skipping...`);
-//         continue;
-//       };
-
-//       const embed = new EventEmbed(key, value, { client: this.client, guild });
-
-//       let message: string | MessagePayload | MessageCreateOptions = {
-//         content: getTitle(key, value),
-//         embeds: [embed],
-//       }
-
-//       const setting = guild.settings.events[key as keyof typeof guild.settings.events];
-
-//       if (setting.enabled) {
-
-//         this.client.logger.info(`Event ${key} is enabled, broadcasting to guild ${guild.id}...`)
-
-//         if (setting.role) {
-//           const role = setting.role as any as Role;
-//           message.content += ` - <@&${role.id}>`;
-//           message.allowedMentions = {
-//             roles: [role.id],
-//           };
-//         }
-
-//         if (setting.schedule) {
-
-//           // const clusterGuild = await getGuild(this.client, guild.id);
-
-//           // if (!clusterGuild) return;
-
-//           // await this.broadcaster.scheduleEvent(clusterGuild, {
-//           //   name: this.getKeyName(key, value, true),
-//           //   entityType: GuildScheduledEventEntityType.External,
-//           //   privacyLevel: GuildScheduledEventPrivacyLevel.GuildOnly,
-//           //   scheduledStartTime: new Date().toLocaleString(),
-//           //   scheduledEndTime: new Date().toLocaleString(),
-//           //   entityMetadata: {
-//           //     location: value.zone && value.territory ? `${value.zone} at ${value.territory}` : '',
-//           //   }
-//           // })
-//         }
-
-//         let channel = setting.channel as any as TextChannel | NewsChannel;
-
-//         if (!channel) {
-//           this.client.logger.info(`Event ${key} has no channel set, skipping...`);
-//           continue;
-//         };
-
-//         this.broadcaster.broadcast(channel, message);
-//       }
-//     }
-//   }
-// }
-
-
 /**
  * Get the event title.
  * 
@@ -230,7 +140,7 @@ function getTitle(key: string, event: Event) {
     case 'boss':
       return `${event.name} appears in ${event.zone} at ${time(event.timestamp, 'T')}`;
     case 'helltide':
-      return `Helltide spawns in ${time(event.timestamp, 'R')} until ${time(event.timestamp + 3600, 'T')}`;
+      return `Helltide spawns in ${time(event.timestamp, 'R')} until ${time(event.timestamp + 3600, 'T')}, next helltide around ${time(event.timestamp + 7200, 'T')}`;
     case 'legion':
       return `Legion spawns in ${time(event.timestamp, 'R')}, next legion at ${time(event.timestamp + 1800, 'T')}`;
     default:
