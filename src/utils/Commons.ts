f69c8eb01e979bca4d978dd9f5df653de2cb871e
Théo <43091603+glazk0@users.noreply.github.com>
2023-06-16 15:37:19 +0200

    Everything is fine

--- src/utils/Commons.ts
+++ src/utils/Commons.ts
@@ -1,10 +1,11 @@
-import { Guild, NewsChannel, Snowflake, TextChannel, TimestampStylesString, time } from 'discord.js';
-import { Logger } from '../lib/Logger';
-import { Client } from '../core/Client';
+import { Guild, NewsChannel, Snowflake, TextChannel, TimestampStylesString, time } from "discord.js";
+import { Logger } from "../lib/Logger";
+import { Client } from "../core/Client";
+import { ARMORY_URL } from "./Constants";
 
 /**
  * Wait for a given amount of milliseconds.
- * 
+ *
  * @param ms - The amount of milliseconds to wait.
  * @returns - Promise<void> The promise.
  */
@@ -25,7 +26,7 @@ export const duration = {
  * Checks if the current environment is development.
  * @type {boolean}
  */
-export const isDev = process.env.NODE_ENV === 'development';
+export const isDev = process.env.NODE_ENV === "development";
 
 /**
  * Returns a url to the armory.
@@ -36,7 +37,7 @@ export const isDev = process.env.NODE_ENV === 'development';
  * @returns - The url to the armory.
  */
 export const getArmoryLink = (battleTag: string, characterId: string) => {
-  return `${process.env.ARMORY_URL}/?account=${battleTag}&hero=${characterId}`;
+  return `${ARMORY_URL}/?account=${battleTag}&hero=${characterId}`;
 };
 
 /**
@@ -47,10 +48,7 @@ export const getArmoryLink = (battleTag: string, characterId: string) => {
  *
  * @returns - The timestamp in the Discord format.
  */
-export const getTimestamp = (
-  timestamp: number,
-  type: TimestampStylesString = 't',
-) => {
+export const getTimestamp = (timestamp: number, type: TimestampStylesString = "t") => {
   return time(timeStampToseconds(timestamp), type);
 };
 
@@ -63,7 +61,6 @@ export const getTimestamp = (
  */
 export const timeStampToseconds = (timestamp: number) => {
   let divisor;
-
 
   if (timestamp > 1_000_000_000_000) divisor = 1_000_000_000;
   else if (timestamp > 1_000_000_000) divisor = 1_000_000;
@@ -81,7 +78,6 @@ export const timeStampToseconds = (timestamp: number) => {
  * @returns The days, hours, minutes and seconds string.
  */
 export const secondsToDhms = (seconds: number) => {
-
   seconds = Number(seconds);
 
   const d = Math.floor(seconds / (3600 * 24));
@@ -89,10 +85,10 @@ export const secondsToDhms = (seconds: number) => {
   const m = Math.floor((seconds % 3600) / 60);
   const s = Math.floor(seconds % 60);
 
-  const dDisplay = d > 0 ? d + (d === 1 ? ' day, ' : ' days, ') : '';
-  const hDisplay = h > 0 ? h + (h === 1 ? ' hour, ' : ' hours, ') : '';
-  const mDisplay = m > 0 ? m + (m === 1 ? ' minute, ' : ' minutes, ') : '';
-  const sDisplay = s > 0 ? s + (s === 1 ? ' second' : ' seconds') : '';
+  const dDisplay = d > 0 ? d + (d === 1 ? " day, " : " days, ") : "";
+  const hDisplay = h > 0 ? h + (h === 1 ? " hour, " : " hours, ") : "";
+  const mDisplay = m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : "";
+  const sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
 
   return dDisplay + hDisplay + mDisplay + sDisplay;
 };
@@ -111,13 +107,13 @@ export const secondsToDhms = (seconds: number) => {
 export async function request(
   url: string,
   json = false,
-  method = 'GET' || 'POST' || 'PUT' || 'DELETE',
+  method = "GET" || "POST" || "PUT" || "DELETE",
   headers = {
-    'User-Agent': 'Lilith/DiscordBot',
+    "User-Agent": "Lilith/DiscordBot",
   },
-  body?: RequestInit['body'],
+  body?: RequestInit["body"]
 ): Promise<any> {
-  if (!url) throw new Error('No URL provided.');
+  if (!url) throw new Error("No URL provided.");
   else {
     let req = null;
 
@@ -127,7 +123,7 @@ export async function request(
         headers,
       };
 
-      if (method !== 'GET') options.body = body;
+      if (method !== "GET") options.body = body;
 
       req = await fetch(url, options);
 
@@ -144,32 +140,30 @@ export async function request(
 
 /**
  * Returns a Discord guild from the cluster.
- * 
+ *
  * @param client - The client.
  * @param guildId - The guild id.
- * 
+ *
  * @returns - The guild.
  */
 export const getGuild = async (client: Client, guildId: Snowflake): Promise<Guild | undefined> => {
-  const evalResult = (await client.cluster
-    .broadcastEval((c, { guildId }) => c.guilds.cache.get(guildId), {
-      context: { guildId },
-    })) as (Guild | undefined)[];
+  const evalResult = (await client.cluster.broadcastEval((c, { guildId }) => c.guilds.cache.get(guildId), {
+    context: { guildId },
+  })) as (Guild | undefined)[];
   return evalResult.find((guild) => guild !== null);
 };
 
 /**
  * Returns a Discord channel from the cluster.
- * 
+ *
  * @param client - The client.
  * @param channelId - The channel id.
- * 
+ *
  * @returns - The channel.
  */
 export const getChannel = async (client: Client, channelId: Snowflake): Promise<NewsChannel | TextChannel | undefined> => {
-  const evalResult = (await client.cluster
-    .broadcastEval((c, { channelId }) => c.channels.fetch(channelId), {
-      context: { channelId },
-    })) as (NewsChannel | TextChannel | undefined)[];
+  const evalResult = (await client.cluster.broadcastEval((c, { channelId }) => c.channels.fetch(channelId), {
+    context: { channelId },
+  })) as (NewsChannel | TextChannel | undefined)[];
   return evalResult.find((channel) => channel !== null);
 };
--- src/utils/embeds/EventEmbed.ts
+++ src/utils/embeds/EventEmbed.ts
@@ -3,13 +3,14 @@ import { Embed } from "./Embed";
 import { Context } from "../../core/Interaction";
 import { Event } from "../../types";
 import { time } from "discord.js";
+import { ARMORY_URL } from "../Constants";
 
 // TODO refactor
 export class EventEmbed extends Embed {
   constructor(key: string, event: Event, ctx: Context) {
     super();
 
-    this.data.url = `${process.env.ARMORY_URL}/events`;
+    this.data.url = `${ARMORY_URL}/events`;
 
     this.data.image = {
       url: getURL(key, event),
@@ -24,11 +25,11 @@ export class EventEmbed extends Embed {
 function getURL(key: string, event: Event) {
   switch (key) {
     case "boss":
-      return `${process.env.ARMORY_URL}/img/territories/${encodeURI(event.territory!)}.webp`;
+      return `${ARMORY_URL}/img/territories/${encodeURI(event.territory!)}.webp`;
     case "helltide":
-      return `${process.env.ARMORY_URL}/img/helltides/${encodeURI(event.zone)}Helltide.webp`;
+      return `${ARMORY_URL}/img/helltides/${encodeURI(event.zone)}Helltide.webp`;
     case "legion":
-      return `${process.env.ARMORY_URL}/img/territories/${encodeURI(event.territory!)}.webp`;
+      return `${ARMORY_URL}/img/territories/${encodeURI(event.territory!)}.webp`;
     default:
       return "https://cdn.discordapp.com/attachments/1117722541209956422/1118197134924185630/no_png.png";
   }
